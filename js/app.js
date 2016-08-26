d5bcecff0a4d50a3ec6a25f35327cc78d67578b0
Mickito <100590093@student.swin.edu.au>
2016-08-26 21:37:32 +1000

    Applied data to table and general fixes

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/Sales', {
-			templateUrl: "templates/Sales.html"
+			templateUrl: "templates/Sales.html",
+			controller:"temporyData"
 		})
         .when('/Items', {
 			templateUrl: "templates/Items.html",
@@ -21,3 +22,11 @@ app.controller('itemsCtrl',
         }
     }
 );
+
+app.controller('temporyData', function ($scope, $http) {
+		$http.get('data/tempData.json')
+			.then(
+				function (response) {
+					$scope.temporyData = response.data;
+				});
+	});
