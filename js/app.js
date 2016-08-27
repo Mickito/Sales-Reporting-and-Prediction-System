4b86f575bd194db6707365248e8677e16e146201
Callum Neideck <cal_neideck@hotmail.com>
2016-08-27 22:22:21 +1000

    Fixed reset button on Items page

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider
-		.when('/Sales', {
-			templateUrl: "templates/Sales.html",
-			controller:"temporyData"
-		})
-		.when('/Stock', {
-			templateUrl: "templates/Items.html",
-			controller: "itemsCtrl"
-		});
+    $routeProvider
+        .when('/Sales', {
+            templateUrl: "templates/Sales.html",
+            controller:"temporyData"
+        })
+        .when('/Stock', {
+            templateUrl: "templates/Items.html",
+            controller: "itemsCtrl"
+        });
 }]);
 
 app.controller('itemsCtrl', function($scope)
 {
-	$scope.items = [];
-	$scope.isEdit = false;
+    $scope.items = [];
+    $scope.isEdit = false;
     $scope.arrayIndex = -1;
-	$scope.onSubmit = function()
-	{
-       
-		item = {};
-		item.name = $scope.itemName;
-		item.price = $scope.itemPrice;
-		item.quantity = $scope.itemQuantity;
-		$scope.items.push(item);
-	}
-	
-	$scope.editItem = function(index)
-	{
+    $scope.onSubmit = function()
+    {
+
+        item = {};
+        item.name = $scope.itemName;
+        item.price = $scope.itemPrice;
+        item.quantity = $scope.itemQuantity;
+        $scope.items.push(item);
+    }
+
+    $scope.editItem = function(index)
+    {
         $scope.isEdit = true;
         $scope.itemName = $scope.items[index].name;
         $scope.itemPrice = $scope.items[index].price;
         $scope.itemQuantity = $scope.items[index].quantity;
         
         $scope.arrayIndex = index;
-	}
+    }
     
     $scope.onUpdate = function()
     {
@@ -46,13 +46,21 @@ app.controller('itemsCtrl', function($scope)
         $scope.items[$scope.arrayIndex].price = $scope.itemPrice;
         $scope.items[$scope.arrayIndex].quantity = $scope.itemQuantity;
     }
+	
+    $scope.onReset = function()
+    {
+        $scope.isEdit = false;
+        $scope.itemName = "";
+        $scope.itemPrice = "";
+        $scope.itemQuantity = "";
+    }
 });
 
 app.controller('temporyData', function ($scope, $http)
 {
-	$http.get('data/tempData.json')
-		.then(
-			function (response) {
-				$scope.temporyData = response.data;
-			});
+    $http.get('data/tempData.json')
+        .then(
+            function (response) {
+                $scope.temporyData = response.data;
+            });
 });
