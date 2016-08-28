7f481ba4560224f3ccc6f27c3634a67ac0984837
Mickito <100590093@student.swin.edu.au>
2016-08-28 20:59:22 +1000

    Created an global object array and removed the .json file.

--- js/app.js
+++ js/app.js
@@ -1,66 +1,75 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/Sales', {
-            templateUrl: "templates/Sales.html",
-            controller:"temporyData"
-        })
-        .when('/Stock', {
-            templateUrl: "templates/Items.html",
-            controller: "itemsCtrl"
-        });
+	$routeProvider
+		.when('/Sales', {
+			templateUrl: "templates/Sales.html"
+			, controller: "saleCtrl"
+		})
+		.when('/Stock', {
+			templateUrl: "templates/Items.html"
+			, controller: "itemsCtrl"
+		});
 }]);
 
-app.controller('itemsCtrl', function($scope)
-{
-    $scope.items = [];
-    $scope.isEdit = false;
-    $scope.arrayIndex = -1;
-    $scope.onSubmit = function()
-    {
+app.run(function ($rootScope) {
+	$rootScope.itemValue = [{
+			name: "Apples"
+			, date: "22/02/1930"
+			, sold: "5"
+			, price: 50.00
+			, quantity: 500
+		}
+		, {
+			name: "Banan"
+			, date: "25/02/1930"
+			, sold: "50"
+			, price: 530.00
+			, quantity: 600
+	}];
 
-        item = {};
-        item.name = $scope.itemName;
-        item.price = $scope.itemPrice;
-        item.quantity = $scope.itemQuantity;
-        $scope.items.push(item);
-    }
-
-    $scope.editItem = function(index)
-    {
-        $scope.isEdit = true;
-        $scope.itemName = $scope.items[index].name;
-        $scope.itemPrice = $scope.items[index].price;
-        $scope.itemQuantity = $scope.items[index].quantity;
-        
-        $scope.arrayIndex = index;
-    }
-    
-    $scope.onUpdate = function()
-    {
-
-        alert($scope.arrayIndex);
-        //update the item through index
-        $scope.items[$scope.arrayIndex].name = $scope.itemName;
-        $scope.items[$scope.arrayIndex].price = $scope.itemPrice;
-        $scope.items[$scope.arrayIndex].quantity = $scope.itemQuantity;
-    }
-	
-    $scope.onReset = function()
-    {
-        $scope.isEdit = false;
-        $scope.itemName = "";
-        $scope.itemPrice = "";
-        $scope.itemQuantity = "";
-    }
 });
 
-app.controller('temporyData', function ($scope, $http)
-{
-    $http.get('data/tempData.json')
-        .then(
-            function (response) {
-                $scope.temporyData = response.data;
-            });
+app.controller('itemsCtrl', function ($scope, $rootScope) {
+	$scope.items = $rootScope.itemValue;
+	$scope.isEdit = false;
+	$scope.arrayIndex = -1;
+
+	$scope.onSubmit = function () {
+
+		item = {};
+		item.name = $scope.itemName;
+		item.price = $scope.itemPrice;
+		item.quantity = $scope.itemQuantity;
+		$scope.items.push(item);
+	}
+
+	$scope.editItem = function (index) {
+		$scope.isEdit = true;
+		$scope.itemName = $scope.items[index].name;
+		$scope.itemPrice = $scope.items[index].price;
+		$scope.itemQuantity = $scope.items[index].quantity;
+
+		$scope.arrayIndex = index;
+	}
+
+	$scope.onUpdate = function () {
+
+		alert($scope.arrayIndex);
+		//update the item through index
+		$scope.items[$scope.arrayIndex].name = $scope.itemName;
+		$scope.items[$scope.arrayIndex].price = $scope.itemPrice;
+		$scope.items[$scope.arrayIndex].quantity = $scope.itemQuantity;
+	}
+
+	$scope.onReset = function () {
+		$scope.isEdit = false;
+		$scope.itemName = "";
+		$scope.itemPrice = "";
+		$scope.itemQuantity = "";
+	}
+});
+
+app.controller('saleCtrl', function ($scope, $rootScope) {
+	$scope.items = $rootScope.itemValue;
 });
